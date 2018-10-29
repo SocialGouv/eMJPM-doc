86257d40f3de4955f78c83fd21cadd0201530c50
Douglas Duteil <douglasduteil@gmail.com>
2018-10-29 20:00:08 +0100

    chore: remove _app css panel image rule

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,11 @@ import App, { Container } from "next/app";
 
 import { Layout } from "../src/components";
 
-import Link from "next/link";
-
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <Container>
-        <style global jsx>{`
-          .panel img {
-            max-width: 100%;
-          }
-        `}</style>
         <Layout>
           <Component {...pageProps} />
         </Layout>
